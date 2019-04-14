a41dce768a2160026f675a5b9f67ae6d7e6801d7
mleu <mleu@atlassian.com>
2019-04-14 04:01:51 +0000

    Add cli

--- main.go
+++ main.go
@@ -1,36 +1,89 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
+	"os"
+
+	"gopkg.in/urfave/cli.v1"
 
 	"github.com/code-server-proxy/proxy"
 	"github.com/gorilla/websocket"
 	"gopkg.in/yaml.v2"
 )
 
+const (
+	readBufferSize  = 4096
+	writeBufferSize = 4096
+	defaultPort     = 5555
+)
+
 func main() {
-	// Config websocket upgrader
-	upgrader := websocket.Upgrader{
-		ReadBufferSize:  4096,
-		WriteBufferSize: 4096,
-	}
-
-	code, err := loadConfig("/opt/go/src/github.com/code-server-proxy/code.yaml")
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	p, err := proxy.NewProxy(
-		proxy.UseUpgrader(upgrader),
-		proxy.UseCode(code),
+	var (
+		logFormat  string
+		bind       string
+		configFile string
 	)
-	if err != nil {
-		log.Fatalln(err)
+
+	app := cli.NewApp()
+	app.Version = "Proxy version 1.0"
+	app.Usage = "Proxy proxies code-server traffic"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "lf,log-format",
+			Destination: &logFormat,
+			Usage:       "--log-format=json can only use json or text",
+			EnvVar:      "LOG_FORMAT",
+			Value:       "json",
+		},
+		cli.StringFlag{
+			Name:        "b, bind",
+			Destination: &bind,
+			EnvVar:      "BIND",
+			Value:       fmt.Sprintf(":%d", defaultPort),
+		},
+		cli.StringFlag{
+			Name:        "c, config",
+			Destination: &configFile,
+			EnvVar:      "CONFIG",
+			Value:       "/opt/go/src/github.com/code-server-proxy/code.yaml",
+		},
 	}
 
-	http.ListenAndServe("127.0.0.1:5555", p)
+	app.Action = func(c *cli.Context) error {
+		// Config websocket upgrader
+		upgrader := websocket.Upgrader{
+			ReadBufferSize:  readBufferSize,
+			WriteBufferSize: writeBufferSize,
+		}
+
+		// Load code-server configs
+		code, err := loadConfig(configFile)
+		if err != nil {
+			return err
+		}
+
+		// Create proxy instance
+		p, err := proxy.NewProxy(
+			proxy.UseUpgrader(upgrader),
+			proxy.UseCode(code),
+		)
+		if err != nil {
+			return err
+		}
+
+		// Serve HTTP request
+		if err = http.ListenAndServe(bind, p); err != nil {
+			return err
+		}
+		return nil
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		os.Exit(1)
+	}
 }
 
 // loadConfig loads the config file of code-server-proxy
